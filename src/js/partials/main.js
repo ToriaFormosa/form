8e9e60726d6f69e061adc928b4410657921a0e71
ArtZ <snickgem@gmail.com>
2021-11-14 23:54:08 +0400

    input range js

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,3 +1,20 @@
 window.addEventListener('load', () => {
+	const rangeInputs = document.querySelectorAll('input[type="range"]');
 
+	rangeInputs.forEach(input => {
+		input.addEventListener('input', function(e) {
+			let target = e.target;
+			const min = target.min;
+			const max = target.max;
+			const val = target.value;
+
+			if (e.target.type !== 'range') {
+				target = document.getElementById('range')
+			}
+
+			target.style.display = 'block';
+			target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%';
+			input.nextElementSibling.querySelector('.js-range-value').textContent = val;
+		});
+	});
 });
